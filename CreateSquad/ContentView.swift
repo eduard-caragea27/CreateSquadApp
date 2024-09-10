49f68f81509422412d67648c7d7f1a4345f8a737
eduard-caragea27 <eduardcaragea9@gmail.com>
2024-09-10 09:27:12 +0300

    Initial Commit

---
+++ CreateSquad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CreateSquad
+//
+//  Created by Eduard Sorin Caragea on 10.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
