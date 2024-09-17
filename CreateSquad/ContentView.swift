4022ea8693f1d2a81b4272d23a4285821f641137
eduardcaragea-27 <eduardcaragea9@gmail.com>
2024-09-17 11:26:14 +0300

    Revert "Implemented the Club Squad screen"
    This reverts commit 096f07fe8cb7436fd6a4829f2edf70ff87814499.

---
+++ CreateSquad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreateSquad
+//
+//  Created by Eduard Sorin Caragea on 10.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("something")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
