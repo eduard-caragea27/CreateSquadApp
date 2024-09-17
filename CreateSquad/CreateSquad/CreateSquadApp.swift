6bcf85d0be51904c701c5424f1a3dacb4cf15156
eduardcaragea-27 <eduardcaragea9@gmail.com>
2024-09-17 11:38:55 +0300

    Implemented Squad Screen

---
+++ CreateSquad/CreateSquad/CreateSquadApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateSquadApp.swift
+//  CreateSquad
+//
+//  Created by Eduard Sorin Caragea on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CreateSquadApp: App {
+    var body: some Scene {
+        WindowGroup {
+           WelcomeScreenView()
+        }
+    }
+}
